8de9bb73c4408e1a5352b65f944fed3fd3436f98
Carl Lin <63276356+xllin0050@users.noreply.github.com>
2022-04-06 11:24:53 +0800

    eslint windiCSS init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
+import legacy from '@vitejs/plugin-legacy'
+import Components from 'unplugin-vue-components/vite'
+import WindiCSS from 'vite-plugin-windicss'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [
+        vue(),
+        eslintPlugin(),
+        legacy({
+            targets: ['defaults', 'not IE 11'],
+        }),
+        Components(),
+        WindiCSS(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 4430,
+    },
 })
